4b132963d207f56c6df13d7aa273cd45b9edcf1c
Liam Clifford <38051927+liam-clifford@users.noreply.github.com>
2023-03-12 16:59:49 -0700

    Create credentials.py

---
+++ google_sheets_spark_connector/credentials.py
@@ -0,0 +1,10 @@
+json_credentials = {
+    "project_id": "YOUR_PROJECT_ID_HERE",
+    "private_key_id": "YOUR_PRIVATE_KEY_ID_HERE",
+    "private_key": "YOUR_PRIVATE_KEY_HERE",
+    "client_email": "YOUR_CLIENT_EMAIL_HERE",
+    "client_id": "YOUR_CLIENT_ID_HERE"
+}
+
+def get_credentials_file():
+    return json_credentials
